3a3ab7f250464582da5f3468cb00b5d43db4916c
Gabriel Teles <31517166+GabrielTBeserra@users.noreply.github.com>
2024-11-02 17:24:23 -0300

    Revert "VAI COM DEUS"
    This reverts commit 770f4a77bed955fe9b836ee9130af254d5809e44.

--- ShareDto/MessageInfoAttribute.cs
+++ ShareDto/MessageInfoAttribute.cs
@@ -1,14 +1,10 @@
-﻿
-namespace GenteTerra.API.Share.ShareDto
+﻿[AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+public class MensagemInfoAttribute : Attribute
 {
-    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
-    public class MensagemInfoAttribute : Attribute
-    {
-        public string Descricao { get; }
+    public string Descricao { get; }
 
-        public MensagemInfoAttribute(string descricao)
-        {
-            Descricao = descricao;
-        }
+    public MensagemInfoAttribute(string descricao)
+    {
+        Descricao = descricao;
     }
 }
